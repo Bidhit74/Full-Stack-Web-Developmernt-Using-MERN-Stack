c24c496d9efe836929c47edc06a77123b91c7344
Bidhit <bidhitkumar61@gmail.com>
2024-08-24 13:35:14 +0530

    Basic JavaScript Code

---
+++ 5 - Advance JavaScript/EventLoop and Asynchronous/app.js
@@ -0,0 +1,45 @@
+// let ev = document.querySelector("h1");
+
+// let print = function () {
+//     console.log("click");
+// };
+
+// ev.addEventListener("click", print);
+// // ev.removeEventListener("click", print);
+
+// let aTag = document.querySelector("a");
+
+// aTag.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     console.log("Click");
+// });
+
+// let Mydiv = document.createElement("div");
+
+// for (let i = 0; i < 100; i++) {
+//     let newElement = document.createElement("p");
+//     newElement.textContent = "This is paragraph" + i;
+//     Mydiv.appendChild(newElement);
+// }
+
+// function print(event) {
+//     console.log(event.target.textContent);
+// }
+
+// Mydiv.addEventListener("click", print);
+
+// document.body.appendChild(Mydiv);
+
+// let element = document.getElementById("wrap");
+
+// element.addEventListener("click", function (e) {
+//     if (e.target.nodeName === "SPAN") {
+//         console.log("Tag clicked" + e.target.textContent);
+//     }
+// });
+
+// for (let i = 0; i < 100; i++) {
+//     let newElement = document.createElement("p");
+//     newElement.textContent = "This is paragraph" + i;
+//     document.body.appendChild(newElement);
+// }
