8dfcf68d205ca988cd5cf4268c0e60e8e00670e9
Bidhit <bidhitkumar61@gmail.com>
2024-09-04 18:23:50 +0530

    setInterval uses

---
+++ 5 - Advance JavaScript/EventLoop and Asynchronous/08-SetInterval.js
@@ -0,0 +1,13 @@
+// let id = setInterval(() => {
+//     console.log("Bidhit");
+// }, 2000);
+
+// console.log(id);
+
+// let id1 = setInterval(() => {
+//     console.log("King");
+// }, 3000);
+
+// console.log(id1);
+
+// clearInterval(id);
