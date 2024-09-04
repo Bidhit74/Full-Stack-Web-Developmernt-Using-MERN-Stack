b1de78fcbc1ae8fcdc4b681e75a331f211541ad2
Bidhit <bidhitkumar61@gmail.com>
2024-09-04 18:18:46 +0530

    Event loops complete

---
+++ 5 - Advance JavaScript/EventLoop and Asynchronous/07-eventLoop2.js
@@ -0,0 +1,54 @@
+// console.log("Hello");
+// setTimeout(() => {console.log("Function")}, 5000);
+// console.log("hy");
+
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let form = document.querySelector("form");
+let para = document.querySelector("p");
+
+// btn.onclick = () => {
+//     console.log("Click");
+// };
+// btn.onmouseenter = () => {
+//     console.log("Onmouseenter Click");
+// };
+
+// btn.addEventListener("click", function () {
+//     console.log(this);
+//     this.style.color = "blue";
+// });
+
+// btn.addEventListener("dblclick", function (e) {
+//     console.log(e);
+// });
+
+// inp.addEventListener("keydown", function (e) {
+//     console.log("Key pressed");
+// });
+// inp.addEventListener("keyup", function (e) {
+//     console.log("Key pressed");
+//     console.log(e.key);
+//     console.log(e.code);
+// });
+
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     // console.dir(inp);
+//     // console.log(inp.value);
+// });
+
+// inp.addEventListener("change", function (e) {
+//     console.log("Change");
+//     console.log("Final value: ", this.value);
+// });
+
+// inp.addEventListener("input", function (e) {
+//     console.log("Input Change");
+//     console.log("Final value: ", this.value);
+// });
+
+// inp.addEventListener("input", function (e) {
+//     console.log(this.value);
+//     para.textContent = this.value;
+// });
