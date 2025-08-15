0e150c63c4e77f0238f6535ef7904cb8da4ad9c5
Bidhit <bidhitkumar61@gmail.com>
2025-08-16 01:08:44 +0530

    Add help.txt and refactor script1.js to comment out folder creation code

--- Backend Development/NodeJS/File System/script1.js
+++ Backend Development/NodeJS/File System/script1.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
 // Create a new folder
+/*
 fs.mkdir("newFolder", (err) => {
     if (err) {
         return console.error("Error creating folder:", err);
     }
     console.log("Folder created successfully!");
+});
+*/
+
+// Read the contents of a folder
+fs.readdir("newFolder", { withFileTypes: true }, (err, files) => {
+    if (err) {
+        return console.error("Error reading folder:", err);
+    }
+    console.log("Contents of the folder:", files);
 });
