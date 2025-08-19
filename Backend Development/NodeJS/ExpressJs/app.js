35ce36120dc351a61f82aa17bd019dd3c244282e
Bidhit <bidhitkumar61@gmail.com>
2025-08-19 17:30:25 +0530

    Connect flash uses and install

--- Backend Development/NodeJS/ExpressJs/app.js
+++ Backend Development/NodeJS/ExpressJs/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import expressSession from "express-session";
+import flass from "connect-flash";
 app.use((req, res, next) => {
     console.log("Middleware function executed");
     next();
@@ -21,6 +22,7 @@ app.get("/about", (req, res) => {
     res.send("About Page");
 });
 
+// express-session example
 app.get("/create", (req, res, next) => {
     req.session.user = "Bidhit Chaudhary"; // Example of setting a session variable
     // Server restart session will be lost
@@ -35,6 +37,23 @@ app.get("/read", (req, res) => {
     }
 });
 
+// connect-flash example
+app.use(flass());
+app.get("/flash", (req, res) => {
+    req.flash("info", "Flash message set!"); // Set a flash message
+    console.log("Flash message created. Redirecting to show it.");
+    res.redirect("/show-flash");
+});
+
+app.get("/show-flash", (req, res) => {
+    const messages = req.flash("info"); // Retrieve flash messages
+    console.log(messages);
+    if (messages.length > 0) {
+        res.send(`Flash message: ${messages[0]}`);
+    } else {
+        res.send("No flash messages found");
+    }
+});
 app.use((req, res) => {
     res.status(404).send("404 Not Found");
 });
