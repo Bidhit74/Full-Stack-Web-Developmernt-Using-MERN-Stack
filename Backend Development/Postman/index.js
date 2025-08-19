282c31410270a64949c8ae1b482ddf3241ee0384
Bidhit <bidhitkumar61@gmail.com>
2025-08-19 23:59:19 +0530

    Top-level Error and Bottom level Error

--- Backend Development/Postman/index.js
+++ Backend Development/Postman/index.js
@@ -1,14 +1,17 @@
 import express from "express";
+// const app = expresssss(); // Top-level Error: 'expresssss' is not defined ---- app crashed - waiting for file changes before starting
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    // res.send(Hello); //Bottom level Erron - ReferenceError --> App not crashed
+    res.send("Hello World");
 });
+
+// Error Handling
 
 app.use((req, res) => {
     res.status(404).send("Not Found");
 });
-
 app.listen(8000, () => {
     console.log("Server is running on http://localhost:8000");
 });
