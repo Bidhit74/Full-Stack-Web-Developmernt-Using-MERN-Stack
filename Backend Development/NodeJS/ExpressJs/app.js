35e7d8b01f3bc0d27d5814469c84673956e476fd
Bidhit <bidhitkumar61@gmail.com>
2025-08-19 17:06:24 +0530

    Express - session uses and install

--- Backend Development/NodeJS/ExpressJs/app.js
+++ Backend Development/NodeJS/ExpressJs/app.js
@@ -1,27 +1,40 @@
 import express from "express";
 const app = express();
-
-// Middleware functions
+import expressSession from "express-session";
 app.use((req, res, next) => {
     console.log("Middleware function executed");
-    // Call the next middleware function in the stack
     next();
 });
 
-// Syntax for defining routes
-// app.get("/route", (req, res) => { ... });
-// app.get("/route", handlerFunction);
+app.use(
+    expressSession({
+        secret: "mySecret",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
 app.get("/", (req, res) => {
-    // middleware function
-    // Middleware function
     res.send("Hello World from Express!");
 });
 app.get("/about", (req, res) => {
     res.send("About Page");
 });
-// Always return a 404 for any other route
-// This should be the last route defined
-// Always place this at the end of your route definitions
+
+app.get("/create", (req, res, next) => {
+    req.session.user = "Bidhit Chaudhary"; // Example of setting a session variable
+    // Server restart session will be lost
+    res.send("Create Page");
+});
+
+app.get("/read", (req, res) => {
+    if (req.session.user) {
+        res.send(`User: ${req.session.user}`);
+    } else {
+        res.send("No user session found");
+    }
+});
+
 app.use((req, res) => {
     res.status(404).send("404 Not Found");
 });
