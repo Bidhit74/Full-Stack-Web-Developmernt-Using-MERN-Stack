61ce2d0a79a4902a277be00c03cecf0ef06ee29c
Bidhit <bidhitkumar61@gmail.com>
2025-08-19 22:41:44 +0530

    basic Get form handling with ejs

--- Backend Development/NodeJS/ExpressJs/serverSideRendring.js
+++ Backend Development/NodeJS/ExpressJs/serverSideRendring.js
@@ -2,9 +2,29 @@ import express from "express";
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     // Render the index.ejs file
     res.render("index", { name: "Bidhit Chaudhary" }); // passing data
+});
+
+app.get("/form", (req, res) => {
+    // Render the form.ejs file
+    res.render("form");
+});
+
+app.get("/checkform", (req, res) => {
+    console.log(req.query); // Accessing query parameters
+    const { name, email } = req.query; // Destructuring query parameters
+    res.send(
+        "<h1>Form Submitted Successfully</h1><p>Name: " +
+            name +
+            "</p><p>Email: " +
+            email +
+            "</p>"
+    );
 });
 
 app.use((req, res) => {
