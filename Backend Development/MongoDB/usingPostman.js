8407eaca9977f4d5c939c9c23fe0672a950e777e
Bidhit <bidhitkumar61@gmail.com>
2025-08-25 00:40:00 +0530

    Implement user creation using Postman

---
+++ Backend Development/MongoDB/usingPostman.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongooseConnection = require("./config/mongoose");
+const userModel = require("./models/user");
+const debuglog = require("debug")("development:usingPostman");
+
+app.use(express.json()); // to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies
+
+app.get("/", (req, res) => {
+    res.send("Hello MongoDB using Postman");
+});
+
+app.post("/create", (req, res) => {
+    res.send(req.body);
+    debuglog("Post request received with postman:", req.body);
+});
+
+app.post("/createUser", async (req, res) => {
+    let { name, username, email, passward } = req.body;
+    let user = await userModel.create({
+        name, // field name and variable name are same
+        username,
+        email,
+        password: passward, // field name and variable name are different
+    });
+    debuglog("User Created", user);
+    res.send(user);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
