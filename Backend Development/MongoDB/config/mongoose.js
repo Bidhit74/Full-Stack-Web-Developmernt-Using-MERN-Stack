d3adb4955ec109efc1dcdb7d90e15537f7e1b32c
Bidhit <bidhitkumar61@gmail.com>
2025-08-24 13:32:29 +0530

    Add debug dependency to package.json

--- Backend Development/MongoDB/config/mongoose.js
+++ Backend Development/MongoDB/config/mongoose.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+const debuglog = require("debug")("development:mongooseconfig");
 
+mongoose.connect("mongodb://localhost:27017/mydatabase");
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+    debuglog(err);
+});
+
+db.on("open", () => {
+    debuglog("Connected to the database");
+});
+
+module.exports = db;
--- Backend Development/MongoDB/index.js
+++ Backend Development/MongoDB/index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
+const mongooseConnection = require("./config/mongoose");
+app.get("/", (req, res) => {
+    res.send("Hello MongoDB");
+});
 
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
