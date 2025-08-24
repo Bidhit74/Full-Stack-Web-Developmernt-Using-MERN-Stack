ec2fe77822c821294472dd326f5b4140968487b6
Bidhit <bidhitkumar61@gmail.com>
2025-08-24 14:24:06 +0530

    Implement user creation endpoint in index.js

--- Backend Development/MongoDB/index.js
+++ Backend Development/MongoDB/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const mongooseConnection = require("./config/mongoose");
+const userModel = require("./models/user");
+const debuglog = require("debug")("development:indexJs");
 app.get("/", (req, res) => {
     res.send("Hello MongoDB");
+});
+
+app.get("/addUser", async (req, res) => {
+    let user = await userModel.create({
+        username: "Bidhit7",
+        name: "Bidhit Chaudhary",
+        email: "bidhit74@gmail.com",
+        password: "bidhit12345",
+    });
+    debuglog("User Created");
+    res.send(user);
 });
 
 app.listen(3000, () => {
