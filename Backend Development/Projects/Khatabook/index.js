7c6fcf33a5125187b7ea7947b46eddb8f3543d24
Bidhit <bidhitkumar61@gmail.com>
2025-08-28 19:47:01 +0530

    feat: Set up Express server with EJS view engine and update dependencies in package.json

--- Backend Development/Projects/Khatabook/index.js
+++ Backend Development/Projects/Khatabook/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const debuglog = require("debug")("development:indexJs");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello Khatabook");
--- Backend Development/Projects/Khatabook/readme.md
+++ Backend Development/Projects/Khatabook/readme.md
@@ -3,17 +3,12 @@
 # 1. Project Setup
 
 -   Install Node.js & npm
-
 -   Create project folder khatabook-backend
-
 -   Initialize: npm init -y
-
 -   Install Express: npm install express
 
 # 2. Basic Server
 
 -   Create server.js
-
 -   Setup Express server with / route (Hello from Khatabook).
-
 -   Run server with nodemon.
