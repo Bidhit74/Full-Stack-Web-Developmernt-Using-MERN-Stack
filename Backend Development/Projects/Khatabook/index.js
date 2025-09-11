9a0d7fb5440b7427dc2da82d1cbe5638caeae355
Bidhit <bidhitkumar61@gmail.com>
2025-09-11 17:33:28 +0530

    feat: Add transaction saving functionality and update form fields in add.ejs

--- Backend Development/Projects/Khatabook/index.js
+++ Backend Development/Projects/Khatabook/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const debuglog = require("debug")("development:indexJs");
 const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,6 +16,20 @@ app.get("/", (req, res) => {
 app.get("/transactions/add", (req, res) => {
     res.render("transactions/add");
     // res.send("Transaction Added Successfully");
+});
+app.post("/transactions/add", (req, res) => {
+    const { date, customer_name } = req.body;
+    fs.writeFile(
+        `./files/${date}` + ".txt",
+        JSON.stringify(req.body),
+        (err) => {
+            if (err) {
+                debuglog("Error writing file:", err);
+                return res.status(500).send("Internal Server Error");
+            }
+        }
+    );
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
