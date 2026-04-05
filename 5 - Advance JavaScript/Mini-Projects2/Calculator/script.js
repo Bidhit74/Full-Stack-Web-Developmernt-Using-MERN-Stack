a15ee14ab53d4c3aa1593eb3f6b8360030232c63
Bidhit <bidhitkumar61@gmail.com>
2026-04-05 21:17:41 +0530

    Add functionality for calculator operation keys and update styles

--- 5 - Advance JavaScript/Mini-Projects2/Calculator/script.js
+++ 5 - Advance JavaScript/Mini-Projects2/Calculator/script.js
@@ -1,8 +1,26 @@
 const numKey = document.querySelectorAll(".num");
 const userInput = document.getElementById("user-input");
+const funcKey = document.querySelectorAll(".func");
 
 numKey.forEach((el) => {
     el.addEventListener("click", () => {
         userInput.value += el.textContent;
+    });
+});
+
+funcKey.forEach((el, index) => {
+    el.addEventListener("click", () => {
+        if (el.textContent === "AC") {
+            userInput.value = "";
+        }
+        if (index === 1) {
+            userInput.value = userInput.value.slice(0, -1);
+        }
+        if (el.textContent === "Sq") {
+            userInput.value *= userInput.value;
+        }
+        if (el.textContent === "%") {
+            userInput.value = userInput.value / 100;
+        }
     });
 });
