846119aa4c52d008ad2f6b1d02bb91b254aa9c41
Bidhit <bidhitkumar61@gmail.com>
2026-04-21 09:18:20 +0530

    Project function working

--- 5 - Advance JavaScript/Mini-Projects2/Calculator/script.js
+++ 5 - Advance JavaScript/Mini-Projects2/Calculator/script.js
@@ -22,5 +22,40 @@ funcKey.forEach((el, index) => {
         if (el.textContent === "%") {
             userInput.value = userInput.value / 100;
         }
+        if (index > 2 && index != 7 && index != 9) {
+            userInput.value += el.textContent;
+        }
+        if (el.textContent === "=") {
+            userInput.value = calculate(userInput.value).toFixed(2);
+        }
     });
 });
+
+function calculate(expr) {
+    const tokens = expr.match(/(\d+|\+|\-|\*|\/)/g);
+    // First handle * and /
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] === "*" || tokens[i] === "/") {
+            const prev = Number(tokens[i - 1]);
+            const next = Number(tokens[i + 1]);
+
+            const result = tokens[i] === "*" ? prev * next : prev / next;
+
+            tokens.splice(i - 1, 3, result);
+            i--;
+        }
+    }
+
+    // Then handle + and -
+    let result = Number(tokens[0]);
+
+    for (let i = 1; i < tokens.length; i += 2) {
+        const operator = tokens[i];
+        const num = Number(tokens[i + 1]);
+
+        if (operator === "+") result += num;
+        if (operator === "-") result -= num;
+    }
+
+    return result;
+}
