dda3047e243909d3287f92411a44abb2d6d6ec1a
Bidhit <bidhitkumar61@gmail.com>
2026-04-21 18:51:31 +0530

    Add CORS middleware and update README with CORS explanation

--- Projects/Full Stack Project/index.js
+++ Projects/Full Stack Project/index.js
@@ -1,9 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
+
+// Use karo production me || kuyki kabhi kabhi localhost me work nahi karta hai
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+        methods: ["GET", "POST", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+// express.json() → JSON data handle karta hai, express.urlencoded() → form data handle karta hai, Dono milke req.body ko usable banate hain
+//Agar extended: false hota → simple data only ❌ nested support nahi
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
--- Projects/Full Stack Project/readme.md
+++ Projects/Full Stack Project/readme.md
@@ -8,3 +8,9 @@
 
 - env file ko GitHub pe upload mat karo
 - gitignore me add karo
+
+## CORS
+
+- CORS is a browser security mechanism that restricts cross-origin HTTP requests.
+
+- CORS ek browser security system hai jo different server se data lene par block karta hai jab tak backend allow na kare
