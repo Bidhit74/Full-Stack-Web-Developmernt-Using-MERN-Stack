ac920e66997181bcad7e4f1f93797f7529893efa
Bidhit <bidhitkumar61@gmail.com>
2026-05-12 10:31:04 +0530

    Change login route from GET to POST for get data from user

--- Projects/Full Stack Project/routes/user.routes.js
+++ Projects/Full Stack Project/routes/user.routes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.get("/verify/:token", verifyUser);
-router.get("/login", loginUser);
+router.post("/login", loginUser);
 
 export default router;
