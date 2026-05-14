3ff079e05c73c183c71a58d764514ed0d7d622d2
Bidhit <bidhitkumar61@gmail.com>
2026-05-14 11:17:37 +0530

    Add profile controller to user exports in user.controller.js

--- Projects/Full Stack Project/controller/user.controller.js
+++ Projects/Full Stack Project/controller/user.controller.js
@@ -1,5 +1,6 @@
 import registerUser from "./register.controller.js";
 import verifyUser from "./verify.controller.js";
 import loginUser from "./login.controller.js";
+import profile from "./profile.controller.js";
 
-export { registerUser, verifyUser, loginUser };
+export { registerUser, verifyUser, loginUser, profile };
