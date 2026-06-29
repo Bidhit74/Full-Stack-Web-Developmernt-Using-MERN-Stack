3d6359937227d0ef40318f1b8d82a1cc6e93c62c
Bidhit <bidhitkumar61@gmail.com>
2026-06-29 14:37:11 +0530

    Some Mistake hai usko sahi karna hai jo kabhi aur karna hai

--- Projects/Authentication with SQL/controllers/auth.controller.js
+++ Projects/Authentication with SQL/controllers/auth.controller.js
@@ -1,4 +1,69 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import dotenv from "dotenv";
+import sendEmail from "../utils/sendMail.js";
+
+const prisma = new PrismaClient();
+dotenv.config();
+
 export const registerUser = async (req, res) => {
-  console.log("User Registered");
-  res.send("Registered");
+  const { email, password, name, phone } = req.body;
+  // validate
+  if (!email || !password || !name || !phone) {
+    console.log("Data missing");
+    return res.status(400).json({
+      success: false,
+      message: "All fields required",
+    });
+  }
+  try {
+    const exitsUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (exitsUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exist",
+      });
+    }
+
+    // hash password using bcrypt
+    const hashPassword = await bcrypt.hash(password, 10);
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        phone,
+        password: hashPassword,
+        verificationToken,
+      },
+    });
+    console.log(user);
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+      });
+    }
+    // send token as email to user
+    await sendEmail(
+      user.email,
+      "Verify your email",
+      `Please click on the following link:
+      ${process.env.BASE_URL}/api/v1/users/verify/${verificationToken}`,
+    );
+
+    // send success status to user
+    res.status(201).json({
+      message: "User registered successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Registration failed",
+    });
+  }
 };
---
+++ Projects/Authentication with SQL/utils/sendMail.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    // transporter
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS,
+      },
+    });
+
+    // send mail
+    await transporter.sendMail({
+      from: process.env.MAILTRAP_SENDER,
+      to,
+      subject,
+      text,
+      html,
+    });
+
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("Email Error:", error.message);
+    throw error;
+  }
+};
+
+export default sendEmail;
