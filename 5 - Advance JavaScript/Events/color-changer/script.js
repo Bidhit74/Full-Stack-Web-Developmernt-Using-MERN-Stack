21d882b1704fb97c97d46f4bd23e8580e2135bb4
Bidhit <bidhitkumar61@gmail.com>
2026-08-13 14:47:25 +0530

    feat: add color changer functionality with HTML and JavaScript

---
+++ 5 - Advance JavaScript/Events/color-changer/script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector("h1");
+const div = document.querySelector("div");
+const btn = document.querySelector("button");
+
+const handleColorChanger = () => {
+  let coordinate1 = Math.floor(Math.random() * 255);
+  let coordinate2 = Math.floor(Math.random() * 255);
+  let coordinate3 = Math.floor(Math.random() * 255);
+  h1.innerText = `rgb(${coordinate1}, ${coordinate2}, ${coordinate3})`;
+  btn.style.backgroundColor = `rgb(${coordinate1}, ${coordinate2}, ${coordinate3})`;
+  div.style.backgroundColor = `rgb(${coordinate1}, ${coordinate2}, ${coordinate3})`;
+};
+
+btn.addEventListener("click", handleColorChanger);
