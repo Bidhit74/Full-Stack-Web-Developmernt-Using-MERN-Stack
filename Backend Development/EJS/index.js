afa4f777a5f36816e424232d5a8073e885a2794a
Bidhit <bidhitkumar61@gmail.com>
2026-08-15 21:57:30 +0530

    Add EJS template files and package configuration
    - Created package.json for EJS project with dependencies and scripts.
    - Added home.ejs template with a greeting and placeholder text.
    - Added roll_dice.ejs template to display a random dice roll result.
    - Initialized jsconfig.json for JavaScript configuration.

---
+++ Backend Development/EJS/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 3030;
+
+// Using EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "/views")); // letest way
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/roll", (req, res) => {
+  // Like DB data comminig
+  let rollD = Math.floor(Math.random() * 6) + 1; // gave 1 to 6
+  res.render("roll_dice.ejs", { rollD });
+});
+app.listen(port, () => {
+  console.log(`Listinig on port: ${port}`);
+});
