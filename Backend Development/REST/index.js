9d2ee3326cba692800b883efe4fa11abf40df8f0
Bidhit <bidhitkumar61@gmail.com>
2026-08-17 14:28:28 +0530

    feat: add edit functionality for posts with method override support(PUT,PATCH & DELETE Request)

--- Backend Development/REST/index.js
+++ Backend Development/REST/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { posts } from "./utils/postsData.js";
 import { v4 as uuidv4 } from "uuid";
+import methodOverride from "method-override";
 const app = express();
 const port = 8000;
 
@@ -15,6 +16,9 @@ app.set("views", path.join(import.meta.dirname, "/views"));
 
 // use public folder
 app.use(express.static(path.join(import.meta.dirname, "public")));
+
+// override with POST having ?_method=DELETE/PUT/PATCH
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hello Bidhit Chaudhary");
@@ -39,6 +43,25 @@ app.get("/posts/:id", (req, res) => {
   const post = posts.find((p) => id == p.id);
   if (post) {
     res.render("show.ejs", { post });
+  } else {
+    res.send("This id does not exist.");
+  }
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((p) => id == p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { content, description } = req.body;
+  const post = posts.find((p) => id == p.id);
+  if (post) {
+    post.content = content;
+    post.description = description;
+    res.redirect("/posts");
   } else {
     res.send("This id does not exist.");
   }
