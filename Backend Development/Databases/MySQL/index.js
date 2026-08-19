9f4741d8a63e19e83523b89da1a924fe807bb8cc
Bidhit <bidhitkumar61@gmail.com>
2026-08-19 23:23:38 +0530

    feat: add EJS templating and basic home view
    - Added EJS as a dependency for templating.
    - Created a new home view (home.ejs) with a greeting message.
    - Added a head include file (head.ejs) for consistent HTML structure.

--- Backend Development/Databases/MySQL/index.js
+++ Backend Development/Databases/MySQL/index.js
@@ -1,8 +1,15 @@
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 import mysql from "mysql2/promise";
 import dotevn from "dotenv";
+import express from "express";
+import path from "path";
 
+const app = express();
 dotevn.config();
+
+// Using EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "/views"));
 
 // Create the connection to database
 const connection = await mysql.createConnection({
@@ -12,37 +19,44 @@ const connection = await mysql.createConnection({
   password: process.env.MY_SQL_PASS,
 });
 
-// let query = "SHOW TABLES";
+let query = "SELECT * FROM user";
 // Using placeholders = "?"
 // Single data add
 // let query = "INSERT INTO user(id,username,email,password) VALUES(?,?,?,?)";
 // let user = [101, "bidhit1", "bkc321@gmail.com", "1234"];
 
-const getRandomUser = () => {
-  return [
-    faker.string.uuid(),
-    faker.internet.username(),
-    faker.internet.email(),
-    faker.internet.password(),
-  ];
-};
+// const getRandomUser = () => {
+//   return [
+//     faker.string.uuid(),
+//     faker.internet.username(),
+//     faker.internet.email(),
+//     faker.internet.password(),
+//   ];
+// };
 
 // Bulk data add
-let query = "INSERT INTO user(id,username,email,password) VALUES ?";
-let users = [];
+// let query = "INSERT INTO user(id,username,email,password) VALUES ?";
+// let users = [];
 
 // Add Random 50 users
-for (let i = 1; i <= 50; i++) {
-  users.push(getRandomUser());
-}
-
+// for (let i = 1; i <= 50; i++) {
+//   users.push(getRandomUser());
+// }
+let data = null;
 try {
-  const [results, fields] = await connection.query(query, [users]);
-  console.log(results); // results contains rows returned by server
-  // console.log(fields); // fields contains extra meta data about results, if available
+  const [results] = await connection.query(query);
+  data = results; // results contains rows returned by server
 } catch (err) {
   console.log(err);
 }
-
 // Close the connection
 await connection.end();
+
+console.log(data);
+app.get("/", (req, res) => {
+  res.render("home.ejs", { data });
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`App listen Port:${process.env.APP_PORT} `);
+});
