4701c062d694afbfc1426d9bfd8ed13fb86a0c5b
Bidhit <bidhitkumar61@gmail.com>
2026-08-19 22:32:12 +0530

    feat: update MySQL query to support multiple user data insertion

--- Backend Development/Databases/MySQL/index.js
+++ Backend Development/Databases/MySQL/index.js
@@ -15,11 +15,19 @@ const connection = await mysql.createConnection({
 // let query = "SHOW TABLES";
 // Using placeholders = "?"
 // Single data add
-let query = "INSERT INTO user(id,username,email,password) VALUES(?,?,?,?)";
-let user = [101, "bidhit1", "bkc321@gmail.com", "1234"];
+// let query = "INSERT INTO user(id,username,email,password) VALUES(?,?,?,?)";
+// let user = [101, "bidhit1", "bkc321@gmail.com", "1234"];
+
+// Multiple data add
+let query = "INSERT INTO user(id,username,email,password) VALUES ?";
+let user = [
+  [102, "bidhit2", "bkc3214@gmail.com", "12345"],
+  [103, "bidhit3", "bkc32414@gmail.com", "123445"],
+  [104, "bidhit4", "bkc32124@gmail.com", "512345"],
+];
 
 try {
-  const [results, fields] = await connection.query(query, user);
+  const [results, fields] = await connection.query(query, [user]);
   console.log(results); // results contains rows returned by server
   // console.log(fields); // fields contains extra meta data about results, if available
 } catch (err) {
