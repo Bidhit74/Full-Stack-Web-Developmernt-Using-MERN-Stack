cebfbdd37736796095add71eb1636c8132f5d3b1
Bidhit <bidhitkumar61@gmail.com>
2026-08-19 14:06:29 +0530

    feat: add initial MySQL setup with faker for random user generation

---
+++ Backend Development/Databases/MySQL/index.js
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+
+const getRandomUser = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
+
+console.log(getRandomUser());
