f55c957e7e38a49c9306c5f8c62f3db4a100c2f6
Bidhit <bidhitkumar61@gmail.com>
2026-08-24 22:34:36 +0530

    feat: updateOne, updateMany, findOneAndUpdate & findByIdAndUpdate

--- Backend Development/Databases/MongoDB/index.js
+++ Backend Development/Databases/MongoDB/index.js
@@ -12,21 +12,21 @@ const connectDB = async () => {
 
 connectDB();
 
-// Create schema
+// *** Create schema ***
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   age: Number,
 });
 
-// Create model
+// *** Create model ***
 const User = mongoose.model("User", userSchema);
 
-// InsertOne Data in collection
+// *** InsertOne Data in collection ***
 // const user1 = new User({ name: "Bidhit", email: "bkc@342gmail.com", age: 24 });
 // const user2 = new User({ name: "Navnit", email: "nkc@342gmail.com", age: 22 });
 
-// InsertMany Data in collection
+// *** InsertMany Data in collection ***
 // User.insertMany([
 //   { name: "Bidhit", email: "bkc@46gmail.com", age: 24 },
 //   { name: "Navnit", email: "k@342gmail.com", age: 22 },
@@ -39,7 +39,7 @@ const User = mongoose.model("User", userSchema);
 //     console.log(err);
 //   });
 
-// Save User
+// *** Save ***
 // user1.save(); // save is async it is return promise(you cam use than and catch method)
 // user2
 //   .save()
@@ -50,6 +50,7 @@ const User = mongoose.model("User", userSchema);
 //     console.log(err);
 //   });
 
+// *** Find ***
 // Find: Model.find() --> returns a Query Object
 // Mongoose Queries are not promise. But they have a ".then() and .catch()" Method
 // User.find().then((res) => {
@@ -66,10 +67,51 @@ const User = mongoose.model("User", userSchema);
 //   });
 
 // Use findOne single user
-User.findOne({ age: { $gt: 22 } })
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// User.findOne({ age: { $gt: 22 } })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// you cal also use : findById("id"); --> this also return Query
+
+// *** Update ***
+// User.updateOne({ name: "Binit" }, { age: 25 })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// User.updateMany({ age: { $gt: 24 } }, { name: "Bidhit" })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Update with see data
+//Previous data see
+// User.findOneAndUpdate({ name: "Lakshmi" }, { age: 18 })
+//Current data see --> Bydefault {new:false}
+// User.findOneAndUpdate({ name: "Lakshmi" }, { age: 19 }, { new: true })
+// With Id
+// User.findByIdAndUpdate(
+//   "6a8c3cb0b68e3958bbed327d",
+//   { age: 17 },
+//   { returnDocument: "after" },
+// )
+// User.findOneAndUpdate(
+//   { name: "Lakshmi" },
+//   { age: 19 },
+//   { returnDocument: "after" },
+// )
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
