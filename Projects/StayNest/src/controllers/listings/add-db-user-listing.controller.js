4f800edb93b2937202ec92c344cd6a87ccfe2c76
Bidhit <bidhitkumar61@gmail.com>
2026-09-08 15:31:44 +0530

    refactor: remove try-catch block from addDBUserListingController for cleaner code and add central middleware

--- Projects/StayNest/src/controllers/listings/add-db-user-listing.controller.js
+++ Projects/StayNest/src/controllers/listings/add-db-user-listing.controller.js
@@ -1,15 +1,10 @@
 import Listing from "../../models/Listing.js";
 
 const addDBUserListingController = async (req, res, next) => {
-	try {
-		const listing = req.body.listing;
-		const newListing = new Listing(listing); // Add New data in DB
-		await newListing.save(); // Save data in DB
-		res.redirect("/listings");
-	} catch (err) {
-		console.log("Error in addDb Listing");
-		return next(err);
-	}
+	const listing = req.body.listing;
+	const newListing = new Listing(listing); // Add New data in DB
+	await newListing.save(); // Save data in DB
+	res.redirect("/listings");
 };
 
 export default addDBUserListingController;
--- Projects/StayNest/src/middlewares/error-handler.middleware.js
+++ Projects/StayNest/src/middlewares/error-handler.middleware.js
@@ -1,4 +1,5 @@
 const handerError = (err, req, res, next) => {
+	console.error("Message:", err.message);
 	res.status(500).render("error.ejs", {
 		message: "Internal Server Error. Please try again later.",
 	});
