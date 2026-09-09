042919c26db696597bad5fb51edb8398d2fe797d
Bidhit <bidhitkumar61@gmail.com>
2026-09-09 14:20:53 +0530

    feat: add validation for listing data in edit and update controllers

--- Projects/StayNest/src/controllers/listings/edit.controller.js
+++ Projects/StayNest/src/controllers/listings/edit.controller.js
@@ -3,6 +3,9 @@ import Listing from "../../models/Listing.js";
 const editController = async (req, res) => {
 	const { id } = req.params;
 	const listing = await Listing.findById(id);
+	if (!listing) {
+			return next(new ExpressError(400, "Send valid id for listings"));
+		}
 	res.render("listings/edit-listing.ejs", { listing });
 };
 
--- Projects/StayNest/src/controllers/listings/update.controller.js
+++ Projects/StayNest/src/controllers/listings/update.controller.js
@@ -2,6 +2,9 @@ import Listing from "../../models/Listing.js";
 const updateController = async (req, res) => {
 	const { id } = req.params;
 	const { listing } = req.body;
+	if (!listing) {
+		return next(new ExpressError(400, "Send valid data for listings"));
+	}
 	await Listing.findByIdAndUpdate(id, listing);
 	res.redirect(`/listings/${id}`);
 };
