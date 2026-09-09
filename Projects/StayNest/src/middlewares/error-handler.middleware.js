ec739b53d6b16034e96d8403205c80defafde052
Bidhit <bidhitkumar61@gmail.com>
2026-09-09 14:17:45 +0530

    feat: add validation for listing data in addDBUserListingController and update error handling in views

--- Projects/StayNest/src/middlewares/error-handler.middleware.js
+++ Projects/StayNest/src/middlewares/error-handler.middleware.js
@@ -1,7 +1,8 @@
 const handerError = (err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message = err.message;
-	console.error(err.stack);
+	// console.error(err.stack);
+	console.log(message);
 	res.status(statusCode).render("error", {
 		statusCode,
 		message,
