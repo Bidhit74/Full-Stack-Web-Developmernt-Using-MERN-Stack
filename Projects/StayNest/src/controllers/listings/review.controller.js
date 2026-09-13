e5dd57e38939d248273d09e12e8cab862bee47ff
Bidhit <bidhitkumar61@gmail.com>
2026-09-13 13:25:47 +0530

    feat: add review functionality with controller and view, and link in listing page

---
+++ Projects/StayNest/src/controllers/listings/review.controller.js
@@ -0,0 +1,5 @@
+const reviewController = async (req, res) => {
+    res.render("listings/review.ejs");
+};
+
+export default reviewController;
--- Projects/StayNest/src/routes/index.routes.js
+++ Projects/StayNest/src/routes/index.routes.js
@@ -8,6 +8,7 @@ import editController from "../controllers/listings/edit.controller.js";
 import updateController from "../controllers/listings/update.controller.js";
 import deleteController from "../controllers/listings/delete.controller.js";
 import validateListing from "../middlewares/validateListing.js";
+import reviewController from "../controllers/listings/review.controller.js";
 
 const router = express.Router();
 
@@ -20,5 +21,6 @@ router.get("/listings/:id", showListing);
 router.put("/listings/:id", updateController);
 router.delete("/listings/:id", deleteController);
 router.get("/listings/:id/edit", editController);
+router.get("/listings/:id/review", reviewController);
 
 export default router;
