3d9bab12befb9c22efc057bfa727c40cf5438007
Bidhit <bidhitkumar61@gmail.com>
2026-09-13 15:00:02 +0530

    feat: enhance listing view to display reviews and average rating

--- Projects/StayNest/src/controllers/listings/show.controller.js
+++ Projects/StayNest/src/controllers/listings/show.controller.js
@@ -1,9 +1,10 @@
 import Listing from "../../models/Listing.js";
 
 const showListing = async (req, res) => {
-	const { id } = req.params;
-	const listing = await Listing.findById(id);
-	res.render("listings/showListing.ejs", { listing });
+    const { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    const { reviews } = listing;
+    res.render("listings/showListing.ejs", { listing, reviews });
 };
 
 export default showListing;
