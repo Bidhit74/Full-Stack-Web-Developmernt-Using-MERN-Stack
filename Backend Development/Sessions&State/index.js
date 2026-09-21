8326b3ed20948ea36e84bb4284fc10dd66ed62c7
Bidhit <bidhitkumar61@gmail.com>
2026-09-21 14:11:32 +0530

    feat: implement session management with express-session and add request count endpoint

--- Backend Development/Sessions&State/index.js
+++ Backend Development/Sessions&State/index.js
@@ -6,11 +6,23 @@ const app = express();
 app.use(
     session({
         secret: "mySecret",
+        resave: false,
+        saveUninitialized: false, // Recommended default // empty session ko save mat karo
+        // saveUninitialized: true, // Use 'true' only when you specifically need a session to be created/saved before you add any data.
     }),
 );
 
 app.get("/", (req, res) => {
     res.send("Hellow Bidhit Chaudhary");
+});
+
+app.get("/request", (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`Request Count = ${req.session.count}`);
 });
 
 app.listen(3000, () => {
