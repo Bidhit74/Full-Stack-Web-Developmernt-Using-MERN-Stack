102dfcb23035405a46b89fe2d9bfe60a9cb0ed4e
Bidhit <bidhitkumar61@gmail.com>
2026-09-22 16:08:32 +0530

    refactor: remove console log for validation error in validateListing middleware

--- Projects/StayNest/src/middlewares/validateListing.js
+++ Projects/StayNest/src/middlewares/validateListing.js
@@ -3,9 +3,7 @@ import ExpressError from "../utils/ExpressError.js";
 
 const validateListing = (req, res, next) => {
     const { error } = listingSchema.validate(req.body);
-
     if (error) {
-        console.log(error.message);
         const message = error.details
             .map((detail) => detail.message)
             .join(", ");
